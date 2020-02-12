2ada8df62e7dcd3359226cbcaa049e74ae6058df
TJ <TJ@DESKTOP-J2LIGQ3>
2020-02-12 15:45:52 +0900

    git

---
+++ HelloJava/src/HelloJava.java
@@ -0,0 +1,14 @@
+//1일차
+public class HelloJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.print("이게 출력문이다\n");
+		System.out.print("이게 출력문이다");
+		System.out.print("이게 출력문이다");
+		
+		//아하 변경되는구나!!
+	}
+
+}
