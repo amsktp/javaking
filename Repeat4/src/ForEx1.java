73fc3c32759e1aafc478a75ca2595eb0adb41465
TJ <TJ@DESKTOP-J2LIGQ3>
2020-02-13 12:31:03 +0900

    반복문 공부

---
+++ Repeat4/src/ForEx1.java
@@ -0,0 +1,24 @@
+//4일차
+//for문
+public class ForEx1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+//		for문
+//		표현식
+//		for (초기화 ; 조건식 ; 증감식) {
+//			조건식이 참일 때 수행될 문장들을 적는다
+//		}
+		for ( int i = 1 ; i < 5 ; i++) {
+			System.out.println(i);
+		}
+		for ( int i = 1 ; i <= 5 ; i++) {
+			System.out.print(i);
+		}
+		
+//		for 안에서 i 를 선언하였기 때문에 밖에서 불가능
+//		System.out.print(i);
+	}
+
+}
